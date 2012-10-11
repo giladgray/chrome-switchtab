fc16221a2797766edb9232ed11cb2840b055ece0
Gilad Gray <giladgray@gmail.com>
2012-10-11 00:59:16 -0700

    comments, styling, enter key

--- popup.js
+++ popup.js
@@ -5,6 +5,7 @@
 var tabs = {}
 chrome.tabs.query({}, function(result) {
   console.log(result);
+  // build a hash of tab HTML elements so we don't have to create new ones all the time
   for (var i =  0; i < result.length; i++) {
     var tab = result[i];
     tabs['' + tab.id] = $("<a>").addClass('tab').attr('href', tab.windowId + "#" + tab.id)
@@ -14,20 +15,30 @@ chrome.tabs.query({}, function(result) {
     		.append($('<div>').addClass('url title').text(tab.url)));
   };
 
-  body = $('#tabs');
+  // for starters, put all tabs in the list
+  body = $('#tabs').html('');
   $.each(tabs, function(index, item) { body.append(item); });
   $('.tab').click(selectTab);
 
-  $('#search').keydown(function(event) {
-    console.log("searching for " + $('#search').val());
+  $('#search').keyup(function(event) {
+    // pressing enter goes to first tab
+    if(event.which == 13) {
+      $('.tab').first().click();
+      return;
+    }
 
+    // filter tabs using ignore-case regex of search term against tab title
+    regex = new RegExp($('#search').val(), 'i')
     list = $('#tabs').text('');
+    console.log("searching for " + regex + '...');
     $.each(tabs, function(index, item) {
-      var title = item.find('.title').text();
-      var regex = new RegExp($('#search').val(), 'i')
-      if(regex.test(title))
+      title = item.find('.title').text();
+      // TODO: also check against URL?
+      if(regex.test(title)) {
         list.append(item);
+      }
     });
+    // configure event handlers for all these new tabs
     list.find('.tab').click(selectTab);
   });
 });
