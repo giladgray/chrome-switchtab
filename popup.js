6af2851ad28eed14b5e7c78196865baa7249b871
Gilad Gray <giladgray@gmail.com>
2012-10-10 23:11:50 -0700

    pretty much works, just fanciness left

--- popup.js
+++ popup.js
@@ -4,11 +4,38 @@
 
 var tabs = {}
 chrome.tabs.query({}, function(result) {
-  tabs = result;
-  console.log(tabs);
-  for (var i =  0; i < tabs.length; i++) {
-    tab = tabs[i];
-    element = $("<div>").addClass('tab').append($('<img>').addClass('favicon').attr('src', tab.favIconUrl)).append($('<span>').addClass('title').text(tab.title));
-    $('body').append(element);
+  console.log(result);
+  for (var i =  0; i < result.length; i++) {
+    var tab = result[i];
+    tabs['' + tab.id] = $("<a>").addClass('tab').attr('href', tab.windowId + "#" + tab.id)
+    	.append($('<img>').addClass('favicon').attr('src', tab.favIconUrl))
+    	.append($('<span>').addClass('details')
+    		.append($('<div>').addClass('title').text(tab.title))
+    		.append($('<div>').addClass('url title').text(tab.url)));
   };
+
+  body = $('#tabs');
+  $.each(tabs, function(index, item) { body.append(item); });
+  $('.tab').click(selectTab);
+
+  $('#search').keydown(function(event) {
+    console.log("searching for " + $('#search').val());
+
+    list = $('#tabs').text('');
+    $.each(tabs, function(index, item) {
+      var title = item.find('.title').text();
+      var regex = new RegExp($('#search').val(), 'i')
+      if(regex.test(title))
+        list.append(item);
+    });
+    list.find('.tab').click(selectTab);
+  });
 });
+
+function selectTab(event) {
+  event.preventDefault();
+  match = /(\d+)#(\d+)/.exec($(event.currentTarget).attr('href'));
+  console.log("Switching to tab " + match[1] + "in window " + match[2]);
+  chrome.windows.update(parseInt(match[1]), {'focused': true});
+  chrome.tabs.update(parseInt(match[2]), {'active': true});
+}
